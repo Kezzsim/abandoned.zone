445af2013dcf514e2852a6a8c73f8bd37ea347c5
Kari Barry <kezzsim@gmail.com>
2023-12-14 15:34:48 -0500

    Animated ✨ Logo

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Abandoned Zone",
+    pageTitle: "abandoned.zone",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -5,15 +5,46 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
   const title = cfg?.pageTitle ?? "Untitled Quartz"
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h1 class={`page-title ${displayClass ?? ""}`}>
+    <h1 data-animated-heading={title} class={`page-title ${displayClass ?? ""}`}>
       <a href={baseDir}>{title}</a>
     </h1>
   )
 }
 
 PageTitle.css = `
+h1::before, h1::after {
+	content: attr(data-animated-heading);
+	position: absolute;
+	top: 0;
+	left: 0;	
+}
+h1::before {
+  filter: hue-rotate(25deg);
+	clip-path: polygon(0% 100%, 100% 100%, 100% 60%, 0 68%)
+}
+@keyframes slide {
+	0% {
+		transform: translateX(0);
+	}
+	50% {
+		transform: translate(-20px, 2%);
+	}
+}
+h1::after {
+	clip-path: polygon(0 0, 100% 0%, 100% 46%, 0 66%);
+	animation: slide 5s infinite;
+}
 .page-title {
   margin: 0;
+  text-align: center;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+}
+.page-title a {
+  color: transparent;
+  display: flex;
 }
 `
 
